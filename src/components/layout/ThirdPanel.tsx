fef27d52b5c28f0079de3a391d99ffbefd04fd41
Abhi <abhiramims333@gmail.com>
2024-12-28 23:41:28 -0500

    Update section 3 with cards layout

--- src/components/layout/ThirdPanel.tsx
+++ src/components/layout/ThirdPanel.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import  Section  from "../../utils/Section";
-import  TextColumn  from "../../utils/TextColumn";
-import  TwoColumns  from "../../utils/TwoColumns";
-import  textcontent  from "../../utils/textcontent";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import Section from "../../utils/Section";
+// import TextColumn from "../../utils/TextColumn";
+// import TwoColumns from "../../utils/TwoColumns";
+// import textcontent from "../../utils/textcontent";
+// import useBaseUrl from "@docusaurus/useBaseUrl";
+import AboutCards from "../../utils/AboutCards";
 
 interface ThirdPanelProps {}
 
- const ThirdPanel: React.FC<ThirdPanelProps> = () => {
-  const imgUrl = useBaseUrl("img/image-03.png");
+const ThirdPanel: React.FC<ThirdPanelProps> = () => {
+  // const imgUrl = useBaseUrl("img/image-03.png");
 
   return (
     <Section className="third-panel" background="light">
-      <TwoColumns
+      <AboutCards />
+
+      {/* <TwoColumns
         columnOne={
           <TextColumn
             title="More Effective Management"
@@ -20,9 +23,9 @@ interface ThirdPanelProps {}
           />
         }
         columnTwo={<img alt="" src={imgUrl} className="third-panel-image" />}
-      />
+      /> */}
     </Section>
   );
 };
 
-export default ThirdPanel
+export default ThirdPanel;
